399159ad9b1c941881e6fb6efae854cbcbdd0602
Timothy Cyrus <tcyrus@tcyr.us>
2020-07-21 17:58:10 -0400

    Update krita.py

--- src/krita.py
+++ src/krita.py
@@ -20,22 +20,33 @@ img_data = {}
 with open(f"{toolspath}/data/picrew.img.data.{imgId}.json") as f:
     img_data = json.load(f)
 
-root = doc.rootNode()
-for cat in cf_data['pList']:
-    name = cat['pNm']
-    defItmId = cat['defItmId']
 
-    cat_node = doc.createGroupLayer(name)
+tmp_pList = []
+for cat in cf_data['pList']:
+    tmp_pList.append({
+        'name': cat['pNm'],
+        'defItmId': cat['defItmId'],
+        'items': cat['items'],
+        'lyrIdx': cf_data['lyrList'][str(cat['lyrs'][0])]
+    })
+
+tmp_pList.sort(key=lambda p: p['lyrIdx'])
+
+root = doc.rootNode()
+for cat in tmp_pList:
+    pName = cat['name']
+    defItmId = cat['defItmId']
+    items = cat['items']
+
+    cat_node = doc.createGroupLayer(pName)
     cat_node.setLocked(True)
-    root.addChildNode(cat_node, None)
 
     for item in cat['items']:
         itemId = item['itmId']
         
         item_node = doc.createGroupLayer(str(itemId))
         item_node.setVisible(itemId == defItmId)
-        cat_node.addChildNode(item_node, None)
-        
+
         parts = list(img_data['lst'][str(itemId)].values())[0]
 
         for name, part in parts.items():
@@ -43,3 +54,7 @@ for cat in cf_data['pList']:
 
             col_node = doc.createFileLayer(name, f"{toolspath}/imgs/{fname}", "None")
             item_node.addChildNode(col_node, None)
+        
+        cat_node.addChildNode(item_node, None)
+
+    root.addChildNode(cat_node, None)
